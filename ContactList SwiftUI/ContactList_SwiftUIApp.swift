4c10c602d1e6b749cf5c9ac5bec6189bb96f0a9e
seregious <seregious@gmail.com>
2022-04-27 10:29:33 +0300

    Initial Commit

---
+++ ContactList SwiftUI/ContactList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUIApp.swift
+//  ContactList SwiftUI
+//
+//  Created by Сергей Иванчихин on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
